737a3e57e3d310ada31d4937a8e3dd9493f0d441
felipe <felipe.carreno@hotmail.com>
2021-01-06 15:43:54 -0300

    arreglo coneccion

--- models/Conexion.php
+++ models/Conexion.php
@@ -7,9 +7,9 @@ class Conexion{
 
   
 
-   public static $user="root";
-    public static $pass="";
-    public static $URL="mysql:host=localhost;dbname=optica_2020";
+   public static $user="uztr3qegaslj6dbl";
+    public static $pass="uztr3qegaslj6dbl";
+    public static $URL="mysql:host=bydpm6mshiwchkaygi24-mysql.services.clever-cloud.com;dbname=bydpm6mshiwchkaygi24";
 
 
 
